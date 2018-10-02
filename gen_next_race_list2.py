f21a31a3af26d0a712e7db57643ea995463c713d
nkymz <nakayama7322@gmail.com>
2018-10-02 13:01:11 +0900

    make POHorseList and it's get-method

---
+++ gen_next_race_list2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import pprint
+
+from ppropkg.pproxls import POHorseList
+
+poh_list = POHorseList()
+
+pprint.pprint(poh_list.get())
--- ppropkg/pproxls.py
+++ ppropkg/pproxls.py
@@ -1,23 +1,32 @@
 # -*- coding: utf-8 -*-
 
 import os
-import re
-import datetime
-import time
 
 import openpyxl
-import requests
-from bs4 import BeautifulSoup
 
 PPRO_PATH = os.getenv("HOMEDRIVE", "None") + os.getenv("HOMEPATH", "None") + "/Dropbox/POG/"
+PO_HORSELIST_NAME = "POG_HorseList.xlsx"
 
 
+class POHorseList:
 
+    def __init__(self):
+        wbpath = (PPRO_PATH + PO_HORSELIST_NAME).replace("\\", "/")
+        self.wb = openpyxl.load_workbook(wbpath)
+        self.ws = self.wb["POHorseList"]
 
+    def get(self):
+        trow = 1
+        mylist = []
 
+        while self.ws.cell(row=trow, column=1).value:
 
-wbpath = (path + "POG_HorseList.xlsx").replace("\\", "/")
-htmlpath = (path + "PO_race_horse_list.html").replace("\\", "/")
+            horse_name = self.ws.cell(row=trow, column=2).value
+            owner_name = self.ws.cell(row=trow, column=1).value
+            horse_id = self.ws.cell(row=trow, column=7).value
+            is_seal = True if self.ws.cell(row=trow, column=6).value == "-" else False
+            mylist.append([horse_id, horse_name, owner_name, is_seal])
 
+            trow += 1
 
-
+        return mylist
